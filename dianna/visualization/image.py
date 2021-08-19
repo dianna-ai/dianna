d8358ba386aa8a0ad998d04305710fc318001133
Christiaan Meijer <c.meijer@esciencecenter.nl>
2021-08-19 11:46:23 +0200

    change default cmaps in visualizer

--- dianna/visualization/image.py
+++ dianna/visualization/image.py
@@ -1,20 +1,21 @@
 import matplotlib.pyplot as plt
 
 
-def plot_image(heatmap, original_data=None, cmap=None):
+def plot_image(heatmap, original_data=None, heatmap_cmap=None, data_cmap=None):
     """
     Example image figure
     """
     # default cmap depends on shape: grayscale or colour
-    if len(heatmap.shape) == 2 and cmap is None:
-        # 2D array, grayscale
-        cmap = 'gray'
 
     fig, ax = plt.subplots()
     alpha = 1
     if original_data is not None:
-        ax.imshow(original_data, cmap=cmap, vmin=0, vmax=1)
+        if len(original_data.shape) == 2 and data_cmap is None:
+            # 2D array, grayscale
+            data_cmap = 'gray'
+
+        ax.imshow(original_data, cmap=data_cmap, vmin=0, vmax=1)
         alpha = .5
 
-    ax.imshow(heatmap, cmap=cmap, alpha=alpha, vmin=0, vmax=1)
+    ax.imshow(heatmap, cmap=heatmap_cmap, alpha=alpha, vmin=0, vmax=1)
     plt.show()
