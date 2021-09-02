5f407b2ab4bf0d6340bf24e1f046d95f67f837cc
Leon Oostrum <l.oostrum@esciencecenter.nl>
2021-09-02 09:58:57 +0200

    Ignore linter error about lowercase function names

--- tests/test_common_usage.py
+++ tests/test_common_usage.py
@@ -6,12 +6,12 @@ model = "Some model"
 input_data = np.ones((5, 3))
 
 
-def test_common_SHAP_pipeline():
+def test_common_SHAP_pipeline():  # noqa: N802 ignore case
     heatmap = dianna.explain(model, input_data, method="SHAP")
     dianna.visualization.plot_image(heatmap, show_plot=False)
     dianna.visualization.plot_image(heatmap, original_data=input_data, show_plot=False)
 
 
-def test_common_LIME_pipeline():
+def test_common_LIME_pipeline():  # noqa: N802 ignore case
     heatmap = dianna.explain(model, input_data, method="LIME")
     dianna.visualization.plot_image(heatmap, show_plot=False)
