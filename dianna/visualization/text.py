0382af6e8730f6da67f2101929c4648a277dd517
Christiaan Meijer <c.meijer@esciencecenter.nl>
2021-09-22 11:19:38 +0200

    refactor: extract method for clarity

--- dianna/visualization/text.py
+++ dianna/visualization/text.py
@@ -6,13 +6,10 @@ def highlight_text(explanation,  # pylint: disable=too-many-arguments
     """
     Highlights text
     Args:
-        output_html_filename:
         explanation: list of tuples of (word, index of word in original data, importance)
         original_data: original text
-        heatmap_cmap:
-        data_cmap:
         show_plot: Shows plot if true (for testing or writing plots to disk instead)
-        output_filename: Name of the file to save the plot to (optional).
+        output_html_filename: Name of the file to save the plot to (optional).
         max_opacity: Maximum opacity (0-1)
 
     Returns:
@@ -25,12 +22,7 @@ def highlight_text(explanation,  # pylint: disable=too-many-arguments
     for word, word_start, importance in sorted(explanation, key=lambda item: item[1]):
         output = output + original_data[current_char:word_start]
 
-        opacity = max_opacity * abs(importance) / max_importance
-        if importance > 0:
-            color = f'rgba(255, 0, 0, {opacity:.2f})'
-        else:
-            color = f'rgba(0, 0, 255, {opacity:2f})'
-        output = output + f'<span style="background:{color}">{word}</span>'
+        output = output + _highlight_word(word, importance, max_importance, max_opacity)
         current_char = word_start + len(word)
 
     if current_char < len(original_data):
@@ -44,3 +36,13 @@ def highlight_text(explanation,  # pylint: disable=too-many-arguments
 
     if show_plot:
         pass
+
+
+def _highlight_word(word, importance, max_importance, max_opacity):
+    opacity = max_opacity * abs(importance) / max_importance
+    if importance > 0:
+        color = f'rgba(255, 0, 0, {opacity:.2f})'
+    else:
+        color = f'rgba(0, 0, 255, {opacity:2f})'
+    highlighted_word = f'<span style="background:{color}">{word}</span>'
+    return highlighted_word
