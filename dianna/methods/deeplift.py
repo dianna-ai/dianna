e0c60858542620fe0faaed28b00c4184dd55049f
Leon Oostrum <l.oostrum@esciencecenter.nl>
2021-10-19 15:47:30 +0200

    Add DeepLift tutorial and make sure inputs to DeepLift have the right shape and type

--- dianna/methods/deeplift.py
+++ dianna/methods/deeplift.py
@@ -21,7 +21,7 @@ class DeepLift:
     def explain_image(self,
                       model,
                       input_data,
-                      baselines=np.array(0),
+                      baselines=None,
                       label=1,
                       additional_forward_args=None,
                       return_convergence_delta=False,
@@ -30,23 +30,27 @@ class DeepLift:
         """
         Run the DeepLift explainer.
 
-        Args:        
+        Args:
             model (str): The path to a ONNX model on disk.
             input_data (np.ndarray): Data to be explained
-            baselines (np.ndarray): Baselines define reference samples that are compared with the inputs. 
+            baselines (np.ndarray): Baselines define reference samples that are compared with the inputs.
             label (int): Index of class to be explained
             additional_forward_args (any, optional):
             return_convergence_delta (bool, optional):
             custom_attribution_func (callable, optional):
 
         Returns:
-            list of (word, index of word in raw text, importance for target class) tuples        
+            list of (word, index of word in raw text, importance for target class) tuples
         """
         # call the deeplift class
         deeplift = DeepLift_captum(model)
         # convert input data from numpy array to torch.tensor
         input_data = torch.from_numpy(input_data).type(torch.FloatTensor)
+        if baselines is None:
+            baselines = np.zeros_like(input_data)
         baselines = torch.from_numpy(baselines).type(torch.FloatTensor)
+        # ensure the label is a native Python int
+        label = int(label)
         # call the attributions
         attributions = deeplift.attribute(input_data,
                                           baselines,
