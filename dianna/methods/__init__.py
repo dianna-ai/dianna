43c55c430e8f092e9f8dd00ee36c3775b80249bf
Leon Oostrum <l.oostrum@esciencecenter.nl>
2021-10-19 16:19:48 +0200

    Fix import order

--- dianna/methods/__init__.py
+++ dianna/methods/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa: F401
+from .deeplift import DeepLift
 from .lime import LIME
 from .rise import RISE
 from .shap import SHAP
-from .deeplift import DeepLift
--- dianna/methods/deeplift.py
+++ dianna/methods/deeplift.py
@@ -1,5 +1,5 @@
-import torch
 import numpy as np
+import torch
 from captum.attr import DeepLift as DeepLift_captum
 
 
