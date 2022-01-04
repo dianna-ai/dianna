e385d91db24ed9e9169081ca961a93d7d4adbb8f
Yang <y.liu@esciencecenter.nl>
2022-01-04 16:37:57 +0100

    first commitment for kernel shap in dianna

--- dianna/methods/shap.py
+++ dianna/methods/shap.py
@@ -1,21 +1,51 @@
 import numpy as np
+import shap
+import onnx
+from skimage.segmentation import slic
+from onnx_tf.backend import prepare # onnx to tf model converter&runner
 
 
-class SHAP:
+class KernelSHAP:
     """
-    SHAP implementation based on captum https://github.com/pytorch/captum
+    Kernel SHAP implementation based on shap https://github.com/slundberg/shap
     """
-    def __init__(self, shap_arg_1=0, shap_arg_2=0):
-        """
-        Example constructor for testing.
-        """
-        self.shap_arg_1 = shap_arg_1
-        self.shap_arg_2 = shap_arg_2
+    def __init__(self):
+        """KernelSHAP initializer.
 
-    def explain_image(self, model, input_data):
+        Args:
+            model (str): The path to a ONNX model on disk.
         """
-        Example call function.
+        
+    def segment_image(self, input_data):
+        """Create segmentation to explain by segment, not every pixel
+
+        Args:
+            model (str): The path to a ONNX model on disk.
+        """        
+        
+
+    def explain_image(self, model):
+        """Run the RISE explainer.
+           The model will be called with masked images,
+           with a shape defined by `batch_size` and the shape of `input_data`
+
+        Args:
+            model (str): The path to a ONNX model on disk.
+
+        Returns:
+            Explanation heatmap for each class (np.ndarray).
         """
-        self.model = model
+        self.model = onnx.load(model)  # load onnx model
 
         return np.zeros(input_data.shape, dtype=float)
+
+    def _mask_image(self):
+        """Define a function that depends on a binary mask representing if an image region is hidden
+
+        """
+
+    def _runner(self):
+        """Define a runner/wrapper to load models and values
+
+        """
+
---
+++ tests/test_kernelshap.py
@@ -0,0 +1,5 @@
+from unittest import TestCase
+
+import dianna
+import numpy as np
+from dianna.methods import shap
