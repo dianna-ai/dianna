b66aaab03c022b5cbf36107f7c3761897176dc2e
Yang <y.liu@esciencecenter.nl>
2022-01-06 11:52:49 +0100

    add test for function _mask_image

--- tests/test_kernelshap.py
+++ tests/test_kernelshap.py
@@ -21,8 +21,16 @@ class ShapOnImages(TestCase):
                                                   0.5, 3, False, 1, None)
         # check segments index
         assert (np.unique(image_segments) == np.arange(1, n_segments)).all
-        # check image shape after segmentation (1st dimension)
+        # check image shape after segmentation
         assert image_segments.shape == input_data[:,:,0].shape
     
-    
-        
+    def test_shap_mask_image(self):
+        input_data = np.random.random((28,28,1))
+        explainer = dianna.methods.KernelSHAP()
+        n_segments=50
+        segments_slic = explainer._segment_image(input_data, n_segments, 10.0,
+                                                 10, 0, None, True, None, True,
+                                                 0.5, 3, False, 1, None)
+        masked_image = explainer._mask_image(np.zeros((1,n_segments)), segments_slic, input_data, 0)
+        # check if all points are masked
+        assert (masked_image[0] == np.zeros(input_data.shape)).all
