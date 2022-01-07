2a83ac8958e8499b84c40e4db38555a7c01ca590
Yang <y.liu@esciencecenter.nl>
2022-01-07 13:14:45 +0100

    clean up the script and fix the channel_axis issue related to segmentation

--- dianna/methods/shap.py
+++ dianna/methods/shap.py
@@ -1,7 +1,6 @@
 import numpy as np
 import shap
 import onnx
-import onnxruntime
 
 # skimage
 from skimage.segmentation import slic
@@ -34,7 +33,7 @@ class KernelSHAP:
         slic_zero,
         start_label,
         mask,
-        channel_axis,
+        channel_axis_first,
     ):
         """Create segmentation to explain by segment, not every pixel
 
@@ -48,6 +47,9 @@ class KernelSHAP:
             via the following link:
             https://scikit-image.org/docs/dev/api/skimage.segmentation.html#skimage.segmentation.slic
         """
+
+        if channel_axis_first:
+            image = np.transpose(image, (1, 2, 0))
 
         image_segments = slic(
             image=image,
@@ -63,7 +65,6 @@ class KernelSHAP:
             slic_zero=slic_zero,
             start_label=start_label,
             mask=mask,
-            channel_axis=channel_axis,
         )
 
         return image_segments
@@ -73,11 +74,11 @@ class KernelSHAP:
         model,
         input_data,
         nsamples="auto",
-        background=None,  # pylint: disable=too-many-arguments
+        background=None,
         n_segments=100,
         compactness=10.0,
         sigma=0,
-        channel_axis=-1,
+        channel_axis_first=False,
         **kwargs,
     ):
         """Run the KernelSHAP explainer.
@@ -92,7 +93,7 @@ class KernelSHAP:
                                      model to explain a model’s prediction on each individual
                                      example. The input dimension must be
                                      [height, width, color_channels] or
-                                     [color_channels, height, width] (see channel_axis)
+                                     [color_channels, height, width] (see channel_axis_first)
             nsamples ("auto" or int): Number of times to re-evaluate the model when
                                       explaining each prediction. More samples lead
                                       to lower variance estimates of the SHAP values.
@@ -105,77 +106,72 @@ class KernelSHAP:
                                square/cubic.
             sigma (float): Width of Gaussian smoothing kernel for pre-processing for
                            each dimension of the image. Zero means no smoothing.
-            channel_axis (int): If None, the image is assumed to be a grayscale (single channel)
-                                image. Otherwise, this parameter indicates which axis of the
-                                array corresponds to channels. Channel_axis was added in version
-                                0.19 of package scikit-image.
+            channel_axis_first (boolean): If True, the shape of the input image is
+                                          [color_channels, height, width]
 
         Other keyword arguments: see the documentation of kernel explainer of SHAP
                                  (also in function "shap_values") via:
         https://github.com/slundberg/shap/blob/master/shap/explainers/_kernel.py
                                  and the documentation of image segmentation via:
         https://scikit-image.org/docs/dev/api/skimage.segmentation.html#skimage.segmentation.slic
-                                 
+
         Returns:
             Explanation heatmap of shapley values for each class (np.ndarray).
         """
-        self.model = onnx.load(model)  # load onnx model
-        self.output_node = prepare(self.model, gen_tensor_dict=True).outputs[0]
+        self.onnx_model = onnx.load(model)  # load onnx model
+        self.output_node = prepare(self.onnx_model, gen_tensor_dict=True).outputs[0]
         self.input_data = input_data
-        self.nsamples = nsamples
+        self.channel_axis_first = channel_axis_first
         self.background = background
-        self.n_segments = n_segments
-        self.compactness = compactness
-        self.sigma = sigma
-        self.channel_axis = channel_axis
         # other keyword arguments for the method segment_image
-        self.max_num_iter = kwargs.get("max_num_iter", 10)
-        self.spacing = kwargs.get("spacing", None)
-        self.convert2lab = kwargs.get("convert2lab", None)
-        self.enforce_connectivity = kwargs.get("enforce_connectivity", True)
-        self.min_size_factor = kwargs.get("min_size_factor", 0.5)
-        self.max_size_factor = kwargs.get("max_size_factor", 3)
-        self.slic_zero = kwargs.get("slic_zero", False)
-        self.start_label = kwargs.get("start_label", 1)
-        self.mask = kwargs.get("mask", None)
+        max_num_iter = kwargs.get("max_num_iter", 10)
+        spacing = kwargs.get("spacing", None)
+        convert2lab = kwargs.get("convert2lab", None)
+        enforce_connectivity = kwargs.get("enforce_connectivity", True)
+        min_size_factor = kwargs.get("min_size_factor", 0.5)
+        max_size_factor = kwargs.get("max_size_factor", 3)
+        slic_zero = kwargs.get("slic_zero", False)
+        start_label = kwargs.get("start_label", 1)
+        mask = kwargs.get("mask", None)
 
         # first check the dimension of input_data
-        if self.input_data.ndim != 3:
+        if input_data.ndim != 3:
             raise IOError(
                 "The input image must follow the required shape [height, width, color_channels] or [color_channels, height, width]"
             )
 
         # call the segment method to create segmentation of input image
         self.image_segments = self._segment_image(
-            self.input_data,
-            self.n_segments,
-            self.compactness,
-            self.max_num_iter,
-            self.sigma,
-            self.spacing,
-            self.convert2lab,
-            self.enforce_connectivity,
-            self.min_size_factor,
-            self.max_size_factor,
-            self.slic_zero,
-            self.start_label,
-            self.mask,
-            self.channel_axis,
+            input_data,
+            n_segments,
+            compactness,
+            max_num_iter,
+            sigma,
+            spacing,
+            convert2lab,
+            enforce_connectivity,
+            min_size_factor,
+            max_size_factor,
+            slic_zero,
+            start_label,
+            mask,
+            channel_axis_first,
         )
 
         # call the Kernel SHAP explainer
-        explainer = shap.KernelExplainer(self._runner, np.zeros((1, self.n_segments)))
+        explainer = shap.KernelExplainer(self._runner, np.zeros((1, n_segments)))
 
         with warnings.catch_warnings():
-            warnings.simplefilter("ignore")  # avoid warnings due to version conflicts
+            # avoid warnings due to version conflicts
+            warnings.simplefilter("ignore")
             shap_values = explainer.shap_values(
-                np.ones((1, self.n_segments)), self.nsamples
+                np.ones((1, n_segments)), nsamples=nsamples
             )
 
         return shap_values
 
     def _mask_image(
-        self, features, segmentation, image, background=None, channel_axis=-1
+        self, features, segmentation, image, background=None, channel_axis_first=False
     ):
         """Define a function that depends on a binary mask representing
            if an image region is hidden.
@@ -190,7 +186,7 @@ class KernelSHAP:
             channel_axis (int): See the function explain_image
         """
         # if the image shape is [color_channels, height, width]
-        if channel_axis != -1:
+        if channel_axis_first:
             image = np.transpose(image, (1, 2, 0))
         # check the background color
         if background is None:
@@ -206,7 +202,7 @@ class KernelSHAP:
             for j in range(features.shape[1]):
                 if features[i, j] == 0:
                     out[i][segmentation == j, :] = background
-        if channel_axis != -1:
+        if channel_axis_first:
             out = np.transpose(out, (0, 3, 1, 2))
 
         return out.astype(np.float32)
@@ -218,13 +214,12 @@ class KernelSHAP:
             features (np.ndarray): A matrix of samples (# samples x # features)
                                    on which to explain the model's output.
         """
-        return prepare(self.model).run(
+        return prepare(self.onnx_model).run(
             self._mask_image(
                 features,
                 self.image_segments,
                 self.input_data,
                 self.background,
-                self.channel_axis,
+                self.channel_axis_first,
             )
         )[f"{self.output_node}"]
-
