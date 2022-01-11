5d74ee0de029872969fd3e359b79a767677986da
Yang <y.liu@esciencecenter.nl>
2022-01-11 15:29:20 +0100

    prepare image data with xarray

--- dianna/methods/shap.py
+++ dianna/methods/shap.py
@@ -11,6 +11,8 @@ class KernelSHAP:
     """
     Kernel SHAP implementation based on shap https://github.com/slundberg/shap
     """
+    # axis labels required to be present in input image data
+    required_labels = ('batch', 'channels')
 
     def __init__(self):
         """KernelSHAP initializer.
@@ -23,7 +25,6 @@ class KernelSHAP:
         n_segments,
         compactness,
         sigma,
-        channel_axis_first,
         **kwargs
     ):
         """Create segmentation to explain by segment, not every pixel
@@ -38,10 +39,6 @@ class KernelSHAP:
             via the following link:
             https://scikit-image.org/docs/dev/api/skimage.segmentation.html#skimage.segmentation.slic
         """
-
-        if channel_axis_first:
-            image = np.transpose(image, (1, 2, 0))
-
         image_segments = skimage.segmentation.slic(
             image=image,
             n_segments=n_segments,
@@ -61,7 +58,7 @@ class KernelSHAP:
         n_segments=100,
         compactness=10.0,
         sigma=0,
-        channel_axis_first=False,
+        axes_labels=None,
         **kwargs,
     ):
         """Run the KernelSHAP explainer.
@@ -71,12 +68,12 @@ class KernelSHAP:
             model (str): The path to a ONNX model on disk.
             input_data (np.ndarray): Data to be explained. It is mandatory to only
                                      provide a single example as input. This is because
-                                     LIME / KernelShap is generally used for sample-based
+                                     KernelShap is generally used for sample-based
                                      interpretability, training a separate interpretable
                                      model to explain a model’s prediction on each individual
                                      example. The input dimension must be
-                                     [height, width, color_channels] or
-                                     [color_channels, height, width] (see channel_axis_first)
+                                     [batch, height, width, color_channels] or
+                                     [batch, color_channels, height, width] (see axes_labels)
             nsamples ("auto" or int): Number of times to re-evaluate the model when
                                       explaining each prediction. More samples lead
                                       to lower variance estimates of the SHAP values.
@@ -87,10 +84,11 @@ class KernelSHAP:
             compactness (int): Balances color proximity and space proximity. Higher values give
                                more weight to space proximity, making superpixel shapes more
                                square/cubic.
-            sigma (float): Width of Gaussian smoothing kernel for pre-processing for
+            sigma (float): Wid th of Gaussian smoothing kernel for pre-processing for
                            each dimension of the image. Zero means no smoothing.
-            channel_axis_first (boolean): If True, the shape of the input image is
-                                          [color_channels, height, width]
+            axes_labels (dict/list, optional): If a dict, key,value pairs of axis index, name.
+                                               If a list, the name of each axis where the index
+                                               in the list is the axis index
 
         Other keyword arguments: see the documentation of kernel explainer of SHAP
                                  (also in function "shap_values") via:
@@ -103,26 +101,19 @@ class KernelSHAP:
         """
         self.onnx_model, self.input_node_dtype,\
         self.output_node = utils.onnx_model_node_loader(model)
-        self.input_data = input_data
-        self.channel_axis_first = channel_axis_first
+        self.axes_labels = axes_labels if axes_labels is not None else []
+        self.input_data = self._prepare_image_data(input_data)
         self.background = background
 
         # other keyword arguments for the method segment_image
         slic_kwargs = utils.get_kwargs_applicable_to_function(skimage.segmentation.slic, kwargs)
 
-        # first check the dimension of input_data
-        if input_data.ndim != 3:
-            raise IOError(
-                "The input image must follow the required shape [height, width, color_channels] or [color_channels, height, width]"
-            )
-
         # call the segment method to create segmentation of input image
         self.image_segments = self._segment_image(
-            input_data,
+            self.input_data,
             n_segments,
             compactness,
             sigma,
-            channel_axis_first,
             **slic_kwargs
         )
 
@@ -138,9 +129,29 @@ class KernelSHAP:
 
         return shap_values, self.image_segments
 
+    def _prepare_image_data(self, input_data):
+        """
+        Transforms the data to be of the shape and type KernelSHAP expects
+        Args:
+            input_data (NumPy-compatible array): Data to be explained
+        Returns:
+            transformed input data
+        """
+        input_data = utils.to_xarray(input_data, self.axes_labels, KernelSHAP.required_labels)
+        # remove batch axis from input data; this is only here for a consistent API
+        # but KernelSHAP wants data without batch axis
+        if not len(input_data['batch']) == 1:
+            raise ValueError("Length of batch axis must be one.")
+        input_data = input_data.sel(batch=0)
+        # ensure channels axis is last and keep track of where it was so we can move it back
+        self.channels_axis_index = input_data.dims.index('channels')
+        input_data = utils.move_axis(input_data, 'channels', -1)
+
+        return input_data
+
     def _mask_image(
         self, features, segmentation, image, background=None,
-        channel_axis_first=False, datatype = np.float32
+        channels_axis_index=2, datatype = np.float32
     ):
         """Define a function that depends on a binary mask representing
            if an image region is hidden.
@@ -152,11 +163,8 @@ class KernelSHAP:
                                        the function _segment_image
             image (np.ndarray): Image to be explained
             background (int): Background color for the masked image
-            channel_axis (int): See the function explain_image
+            channel_axis_index (int): See the function _prepare_image_data
         """
-        # if the image shape is [color_channels, height, width]
-        if channel_axis_first:
-            image = np.transpose(image, (1, 2, 0))
         # check the background color
         if background is None:
             background = image.mean((0, 1))
@@ -171,7 +179,9 @@ class KernelSHAP:
             for j in range(features.shape[1]):
                 if features[i, j] == 0:
                     out[i][segmentation == j, :] = background
-        if channel_axis_first:
+        
+        # the output shape should satisfy the requirement from onnx model input shape
+        if channels_axis_index != 2:
             out = np.transpose(out, (0, 3, 1, 2))
 
         return out.astype(datatype)
@@ -189,7 +199,7 @@ class KernelSHAP:
                 self.image_segments,
                 self.input_data,
                 self.background,
-                self.channel_axis_first,
+                self.channels_axis_index,
                 self.input_node_dtype.as_numpy_dtype
             )
         )[f"{self.output_node}"]
--- tests/test_kernelshap.py
+++ tests/test_kernelshap.py
@@ -15,13 +15,11 @@ class ShapOnImages(TestCase):
         n_segments = 50
         compactness = 10.0
         sigma = 0
-        channel_axis_first = False
         image_segments = explainer._segment_image(
             input_data,
             n_segments,
             compactness,
             sigma,
-            channel_axis_first
         )
         # check segments index
         assert np.amax(image_segments) <= n_segments
@@ -29,48 +27,31 @@ class ShapOnImages(TestCase):
         assert image_segments.shape == input_data[:, :, 0].shape
 
     def test_shap_mask_image(self):
-        # check image with channel axis = -1
         input_data = np.random.random((28, 28, 1))
         explainer = dianna.methods.KernelSHAP()
         n_segments = 50
         compactness = 10.0
         sigma = 0
-        channel_axis_first = False
+        background=0,
         segments_slic = explainer._segment_image(
             input_data,
             n_segments,
             compactness,
             sigma,
-            channel_axis_first
         )
         masked_image = explainer._mask_image(
-            np.zeros((1, n_segments)), segments_slic, input_data, 0, False
-        )
-        # check if all points are masked
-        assert np.array_equal(masked_image[0], np.zeros(input_data.shape))
-
-        # check image with channel axis = 0
-        input_data = np.random.random((1, 28, 28))
-        channel_axis_first = True
-        segments_slic = explainer._segment_image(
-            input_data,
-            n_segments,
-            compactness,
-            sigma,
-            channel_axis_first
-        )
-        masked_image = explainer._mask_image(
-            np.zeros((1, n_segments)), segments_slic, input_data, 0, True
+            np.zeros((1, n_segments)), segments_slic, input_data, background=0,
         )
         # check if all points are masked
         assert np.array_equal(masked_image[0], np.zeros(input_data.shape))
 
     def test_shap_explain_image(self):
-        input_data = np.random.random((1, 28, 28))
+        input_data = np.random.random((1, 1, 28, 28))
         onnx_model_path = "./tests/test_data/mnist_model.onnx"
         n_segments = 50
         explainer = KernelSHAP()
-        shap_values, segments_slic = explainer.explain_image(
+        axes_labels = ('batch', 'channels', 'height', 'width')
+        shap_values, _ = explainer.explain_image(
             onnx_model_path,
             input_data,
             nsamples=1000,
@@ -78,7 +59,7 @@ class ShapOnImages(TestCase):
             n_segments=n_segments,
             compactness=10.0,
             sigma=0,
-            channel_axis_first=True,
+            axes_labels=axes_labels,
         )
 
         assert shap_values[0].shape == np.zeros((1, n_segments)).shape
