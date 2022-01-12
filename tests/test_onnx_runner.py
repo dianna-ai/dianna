446700ceec85885f81dcdbca0abc983900bc2456
Christiaan Meijer <c.meijer@esciencecenter.nl>
2022-01-12 16:36:31 +0100

    add docstrings refs #74

--- tests/test_onnx_runner.py
+++ tests/test_onnx_runner.py
@@ -3,10 +3,12 @@ from dianna.utils.onnx_runner import SimpleModelRunner
 
 
 def generate_data(batch_size):
+    """Generate a batch of random data."""
     return np.random.randint(0, 256, size=(batch_size, 1, 28, 28))  # MNIST shape
 
 
 def test_onnx_runner():
+    """Tests if the onnx runner can run a test model and make predictions."""
     filename = 'tests/test_data/mnist_model.onnx'
     n_classes = 2  # binary MNIST model
     batch_size = 3
