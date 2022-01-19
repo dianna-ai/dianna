a8a11ea0cd24d2ae27eb950626e90bf9acc4cdd9
Christiaan Meijer <c.meijer@esciencecenter.nl>
2022-01-19 17:06:23 +0100

    keep original user settings in object don't overwrite original settings and store the active settings in a temporary variable instead.

--- dianna/methods/rise.py
+++ dianna/methods/rise.py
@@ -57,12 +57,12 @@ class RISE:
         runner = utils.get_function(model_or_function, preprocess_function=self.preprocess_function)
         input_tokens = np.asarray(model_or_function.tokenizer(input_text))
         text_length = len(input_tokens)
-        self.p_keep = self._determine_p_keep_for_text(input_tokens, runner) if self.p_keep is None else self.p_keep
+        active_p_keep = self._determine_p_keep_for_text(input_tokens, runner) if self.p_keep is None else self.p_keep
         input_shape = (text_length,)
-        self.masks = self._generate_masks_for_text(input_shape, self.p_keep,
+        self.masks = self._generate_masks_for_text(input_shape, active_p_keep,
                                                    self.n_masks)  # Expose masks for to make user inspection possible
         sentences = self._create_masked_sentences(input_tokens, self.masks)
-        saliencies = self._get_saliencies(runner, sentences, text_length, batch_size, self.p_keep)
+        saliencies = self._get_saliencies(runner, sentences, text_length, batch_size, active_p_keep)
         return self._reshape_result(input_tokens, labels, saliencies)
 
     def _determine_p_keep_for_text(self, input_data, runner, n_masks=100):
@@ -142,12 +142,12 @@ class RISE:
         input_data, full_preprocess_function = self._prepare_image_data(input_data)
         runner = utils.get_function(model_or_function, preprocess_function=full_preprocess_function)
 
-        self.p_keep = self._determine_p_keep_for_images(input_data, runner) if self.p_keep is None else self.p_keep
+        active_p_keep = self._determine_p_keep_for_images(input_data, runner) if self.p_keep is None else self.p_keep
 
         # data shape without batch axis and channel axis
         img_shape = input_data.shape[1:3]
         # Expose masks for to make user inspection possible
-        self.masks = self.generate_masks_for_images(img_shape, self.p_keep, self.n_masks)
+        self.masks = self.generate_masks_for_images(img_shape, active_p_keep, self.n_masks)
 
         # Make sure multiplication is being done for correct axes
         masked = input_data * self.masks
@@ -158,7 +158,7 @@ class RISE:
         self.predictions = np.concatenate(batch_predictions)
 
         saliency = self.predictions.T.dot(self.masks.reshape(self.n_masks, -1)).reshape(-1, *img_shape)
-        result = normalize(saliency, self.n_masks, self.p_keep)
+        result = normalize(saliency, self.n_masks, active_p_keep)
         if labels is not None:
             result = result[list(labels)]
         return result
