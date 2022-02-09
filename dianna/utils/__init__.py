bb37135eaa1f55575f7073519aaf79b7aa661e16
E. G. Patrick Bos <egpbos@gmail.com>
2022-02-09 17:23:24 +0100

    revert change in utils/__init__.py

--- dianna/utils/__init__.py
+++ dianna/utils/__init__.py
@@ -1,2 +1,6 @@
 # flake8: noqa: F401
-from .misc import *
+from .misc import get_function
+from .misc import get_kwargs_applicable_to_function
+from .misc import move_axis
+from .misc import onnx_model_node_loader
+from .misc import to_xarray
