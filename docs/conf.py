02a9216b424bbb8abb8814fa909950699f48cf14
Christiaan Meijer <c.meijer@esciencecenter.nl>
2022-12-01 15:21:27 +0100

    bump version to 0.4.2

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = u"DIANNA Team"
 # built documents.
 #
 # The short X.Y version.
-version = "0.4.1"
+version = "0.4.2"
 # The full version, including alpha/beta/rc tags.
 release = version
 
