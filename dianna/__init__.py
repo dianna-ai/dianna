b726da5a22ff00eb2b4e4a4485b4b1b1435b3d7d
Leon Oostrum <l.oostrum@esciencecenter.nl>
2023-01-31 15:19:41 +0100

    Bump version to 0.7.0

--- dianna/__init__.py
+++ dianna/__init__.py
@@ -30,7 +30,7 @@ logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __author__ = "DIANNA Team"
 __email__ = "dianna-ai@esciencecenter.nl"
-__version__ = "0.6.0"
+__version__ = "0.7.0"
 
 
 def explain_image(model_or_function, input_data, method, labels, **kwargs):
