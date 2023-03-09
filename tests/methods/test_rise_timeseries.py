201f57dda845a32a142e1f886a2060995e1c923c
Christiaan Meijer <c.meijer@esciencecenter.nl>
2023-03-09 15:50:06 +0100

    add rise input-output shape test (#refs 478)

---
+++ tests/methods/test_rise_timeseries.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+
+import numpy as np
+
+import dianna
+import dianna.visualization
+from tests.utils import run_model
+
+
+
+def test_rise_timeseries_correct_output_shape():
+    """Test if rise runs and outputs the correct shape given some data and a model function."""
+    input_data = np.random.random((10, 1))
+    axis_labels = ['t', 'channels']
+    labels = [1]
+
+    heatmaps = dianna.explain_timeseries(run_model, input_data, "RISE", labels, axis_labels=axis_labels,
+                                         n_masks=200, p_keep=.5)
+
+    assert heatmaps.shape == (len(labels), *input_data.shape)
