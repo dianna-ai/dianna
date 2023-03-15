6d84714b6d1d6d4f34ff5be4746cdcdd62fa7eb6
Laura Ootes <l.ootes@esciencecenter.nl>
2023-03-15 14:15:27 +0100

    create label file for ResNet model

---
+++ dashboard/app_data/get_resnet50_classnames.py
@@ -0,0 +1,8 @@
+# write all class labels for the ResNet50 model to a txt file
+import numpy as np
+from tensorflow.keras.applications.resnet50 import decode_predictions
+
+class_names = [decode_predictions(np.eye(1,1000, i))[0][0][1] for i in range(0, 1000)]
+
+with open('labels_resnet50.txt','w') as file:
+	file.write('\n'.join(class_names))
