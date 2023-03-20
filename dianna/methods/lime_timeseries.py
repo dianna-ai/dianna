961cf022680e5092d812153bd2b17bd97e2f879f
Yang <y.liu@esciencecenter.nl>
2023-03-20 17:27:16 +0100

    create class for lime for timeseries

---
+++ dianna/methods/lime_timeseries.py
@@ -0,0 +1,39 @@
+#from lime import explanation
+from lime import lime_base
+import sklearn
+from dianna.utils.maskers import generate_masks
+from dianna.utils.maskers import mask_data
+
+
+class LimeTimeseries:
+    """LIME implementation for timeseries.
+    
+    This implementation is inspired by the paper:
+    Validation of XAI explanations for multivariate time series classification in
+    the maritime domain. (https://doi.org/10.1016/j.jocs.2021.101539)
+    """
+    def __init__(self):
+        """Initializes Lime explainer for timeseries."""
+        pass
+
+    def explain(self, 
+                model_or_function, 
+                input_data,
+                labels,
+                num_features,
+                num_samples,
+                num_slices,
+                mask_type='mean',
+                ):  # pylint: disable=too-many-arguments,too-many-locals
+        """Run the LIME explainer for timeseries.
+        """
+        # TODO: p_keep does not exist in LIME, we should remove it after adapting
+        #       maskers function to LIME.
+        masks = generate_masks(input_data, num_samples, p_keep=0.9)
+        masked = mask_data(input_data, masks, mask_type='mean')
+        distance = self._calculate_distance(input_data, masked)
+        # implementation for reference
+        # https://github.com/emanuel-metzenthin/Lime-For-Time/blob/3af530f778ab2593246cefc1e5fdb28fa872dbdf/lime_timeseries.py#L130
+        # TODO: scores =  lime_base.explain_instance_with_data()
+        
+        raise NotImplementedError
