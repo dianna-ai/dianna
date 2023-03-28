bdda754b5bd36edba64706ea3824cb44c6c5a39b
Christiaan Meijer <c.meijer@esciencecenter.nl>
2023-03-28 11:42:10 +0200

    add rise timeseries test with expert model

--- tests/methods/test_rise_timeseries.py
+++ tests/methods/test_rise_timeseries.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 import dianna
+from tests.methods.time_series_test_case import input_train_mean, \
+    average_temperature_timeseries_with_1_cold_and_1_hot_day, run_expert_model
 from tests.utils import run_model
 
 
@@ -14,3 +16,21 @@ def test_rise_timeseries_correct_output_shape():
                                          n_masks=200, p_keep=.5)
 
     assert heatmaps.shape == (len(labels), *input_data.shape)
+
+
+def test_rise_timeseries_with_expert_model_for_correct_max_and_min():
+    hot_day_index = 6
+    cold_day_index = 12
+    temperature_timeseries = average_temperature_timeseries_with_1_cold_and_1_hot_day(cold_day_index, hot_day_index)
+
+    summer_explanation, winter_explanation = dianna.explain_timeseries(run_expert_model,
+                                                                       timeseries_data=temperature_timeseries,
+                                                                       method='rise',
+                                                                       labels=[0, 1],
+                                                                       p_keep=0.1, n_masks=10000,
+                                                                       mask_type=input_train_mean)
+
+    assert np.argmax(summer_explanation) == hot_day_index
+    assert np.argmin(summer_explanation) == cold_day_index
+    assert np.argmax(winter_explanation) == cold_day_index
+    assert np.argmin(winter_explanation) == hot_day_index
