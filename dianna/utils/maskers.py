289275fa7e2efde9764dd248cbf96fdfc8799871
Christiaan Meijer <c.meijer@esciencecenter.nl>
2023-04-13 16:15:00 +0200

    formatted imports

--- dianna/utils/maskers.py
+++ dianna/utils/maskers.py
@@ -1,6 +1,5 @@
 import warnings
 from typing import Union
-
 import numpy as np
 
 
--- tests/methods/test_maskers.py
+++ tests/methods/test_maskers.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pytest
-
-from dianna.utils.maskers import (generate_channel_masks, generate_masks,
-                                  mask_data)
+from dianna.utils.maskers import generate_channel_masks
+from dianna.utils.maskers import generate_masks
+from dianna.utils.maskers import mask_data
 
 
 def test_mask_has_correct_shape_univariate():
