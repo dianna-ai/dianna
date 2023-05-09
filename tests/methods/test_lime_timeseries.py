7d091e345257fe0715a7a363d3fa96affc79f5f0
Yang <y.liu@esciencecenter.nl>
2023-05-09 15:45:22 +0200

    fix tests

--- tests/methods/test_lime_timeseries.py
+++ tests/methods/test_lime_timeseries.py
@@ -34,22 +34,21 @@ class LIMEOnTimeseries(TestCase):
         masked = mask_data(dummy_timeseries, masks, mask_type="mean")
         explainer = LIMETimeseries()
         distance = explainer._calculate_distance(
-            dummy_timeseries, masked.reshape((-1, 50)), distance_method="cosine"
+            masked.reshape((-1, 50)), distance_method="cosine"
         )
         assert len(distance) == number_of_masks
 
     def test_dtw_distance(self):
         """Test DTW distance."""
         # Create some test data
-        input_data = np.array([1, 2, 3, 4, 5])
-        masked_data = np.array([[1, 2, 0, 0, 5], [0, 0, 3, 4, 5]])
+        masked_data = np.array([[1, 2, 3, 4, 5], [0, 0, 3, 4, 5]])
 
         # Calculate the expected results
-        expected_distance = np.array([6.0, 3.0])
+        expected_distance = np.array([0.0, 3.0])
 
         # Calculate the actual results
         explainer = LIMETimeseries()
-        actual_distance = explainer._dtw_distance(input_data, masked_data)
+        actual_distance = explainer._dtw_distance(masked_data)
 
         # Check that the actual and expected results are equal
         np.testing.assert_array_almost_equal(actual_distance, expected_distance)
