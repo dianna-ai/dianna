bd06da7e4836555d24fbf7c477bd6e45ed54157b
Yang <y.liu@esciencecenter.nl>
2023-05-10 13:34:47 +0200

    add/update tests

--- tests/methods/test_lime_timeseries.py
+++ tests/methods/test_lime_timeseries.py
@@ -26,8 +26,8 @@ class LIMEOnTimeseries(TestCase):
         )
         assert len(exp.local_exp[1]) == num_features
 
-    def test_cosine_distance(self):
-        """Test cosine distance."""
+    def test_distance_shape(self):
+        """Test the shape of returned distance array."""
         dummy_timeseries = np.random.random((50, 1))
         number_of_masks = 50
         masks = generate_masks(dummy_timeseries, number_of_masks, p_keep=0.9)
@@ -38,6 +38,32 @@ class LIMEOnTimeseries(TestCase):
         )
         assert len(distance) == number_of_masks
 
+    def test_cosine_euclidean_distance(self):
+        """Test the calculation of cosine and euclidean distance."""
+        # Create some test data
+        masked_data = np.array([[1, 2, 3, 4, 5], [0, 0, 3, 4, 5]])
+
+        # Calculate the expected results
+        expected_cosine_distance = np.array([0.0, 4.653741])
+        expected_euclidean_distance = np.array([0.0, 2.236068])
+
+        # Calculate the distance
+        explainer = LIMETimeseries()
+
+        distance_cosine = explainer._calculate_distance(
+            masked_data, distance_method="cosine"
+        )
+
+        distance_euclidean = explainer._calculate_distance(
+            masked_data, distance_method="euclidean"
+        )
+
+        # Check that the calculated and expected results are equal
+        np.testing.assert_array_almost_equal(
+            [expected_cosine_distance, expected_euclidean_distance],
+            [distance_cosine, distance_euclidean],
+        )
+
     def test_dtw_distance(self):
         """Test DTW distance."""
         # Create some test data
@@ -46,9 +72,9 @@ class LIMEOnTimeseries(TestCase):
         # Calculate the expected results
         expected_distance = np.array([0.0, 3.0])
 
-        # Calculate the actual results
+        # Calculate the distance
         explainer = LIMETimeseries()
-        actual_distance = explainer._dtw_distance(masked_data)
+        distance = explainer._dtw_distance(masked_data)
 
-        # Check that the actual and expected results are equal
-        np.testing.assert_array_almost_equal(actual_distance, expected_distance)
+        # Check that the calculated and expected results are equal
+        np.testing.assert_array_almost_equal(distance, expected_distance)
