ebefbe8d6a2dad4fb37db21555304e157e901c54
Yang <y.liu@esciencecenter.nl>
2023-12-12 12:52:05 +0100

    add tests for the shap tabular api

---
+++ tests/methods/test_shap_tabular.py
@@ -0,0 +1,35 @@
+"""Test LIME tabular method."""
+from unittest import TestCase
+import numpy as np
+import dianna
+from dianna.methods.kernelshap_tabular import KERNELSHAPTabular
+from tests.utils import run_model
+
+
+class LIMEOnTabular(TestCase):
+    """Suite of LIME tests for the tabular case."""
+
+    def test_shap_tabular_classification_correct_output_shape(self):
+        """Test the output of explainer."""
+        training_data = np.random.random((10, 2))
+        input_data = np.random.random(2)
+        feature_names = ["feature_1", "feature_2"]
+        explainer = KERNELSHAPTabular(training_data,
+                                      mode ='classification',
+                                      feature_names=feature_names,)
+        exp = explainer.explain(
+            run_model,
+            input_data,
+        )
+        assert len(exp[0]) == len(feature_names)
+
+    def test_shap_tabular_regression_correct_output_shape(self):
+        """Test the output of explainer."""
+        training_data = np.random.random((10, 2))
+        input_data = np.random.random(2)
+        feature_names = ["feature_1", "feature_2"]
+        exp = dianna.explain_tabular(run_model, input_tabular=input_data, method='kernelshap',
+                                     mode ='regression', training_data = training_data,
+                                     training_data_kmeans = 2, feature_names=feature_names)
+
+        assert len(exp) == len(feature_names)
