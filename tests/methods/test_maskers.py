d7d866c7765f094c527d9396190f5f7c41ec61bd
Christiaan Meijer <c.meijer@esciencecenter.nl>
2024-03-26 11:47:40 +0100

    rename timeseries mask tests to clearify the data domain

--- tests/methods/test_maskers.py
+++ tests/methods/test_maskers.py
@@ -9,7 +9,7 @@ from dianna.utils.maskers import generate_timeseries_masks
 from dianna.utils.maskers import mask_data
 
 
-def test_mask_has_correct_shape_univariate():
+def test_timeseries_mask_has_correct_shape_univariate():
     """Test masked data has the correct shape for a univariate input."""
     input_data = _get_univariate_time_series()
     number_of_masks = 5
@@ -19,7 +19,7 @@ def test_mask_has_correct_shape_univariate():
     assert result.shape == tuple([number_of_masks] + list(input_data.shape))
 
 
-def test_mask_has_correct_type_univariate():
+def test_timeseries_mask_has_correct_type_univariate():
     """Test masked data has the correct dtype for a univariate input."""
     input_data = _get_univariate_time_series()
     number_of_masks = 5
@@ -56,13 +56,13 @@ def test_generate_segmented_time_step_masks_dtype_multivariate():
     assert result.dtype == bool
 
 
-def test_mask_has_correct_shape_multivariate():
+def test_timeseries_mask_has_correct_shape_multivariate():
     """Test masked data has the correct shape for a multivariate input."""
     input_data = _get_multivariate_time_series()
     number_of_masks = 5
 
-    result = _call_masking_function(input_data,
-                                    number_of_masks=number_of_masks)
+    result = _call_timeseries_masking_function(input_data,
+                                               number_of_masks=number_of_masks)
 
     assert result.shape == tuple([number_of_masks] + list(input_data.shape))
 
@@ -76,24 +76,24 @@ def test_mask_has_correct_shape_multivariate():
         (0.5, 0.5),
         (0.99, 0.9),  # Mask only 1
     ])
-def test_mask_contains_correct_number_of_unmasked_parts(
+def test_timeseries_mask_contains_correct_number_of_unmasked_parts(
         p_keep_and_expected_rate):
     """Number of unmasked parts should be conforming the given p_keep."""
     p_keep, expected_rate = p_keep_and_expected_rate
     input_data = _get_univariate_time_series()
 
-    result = _call_masking_function(input_data, p_keep=p_keep)
+    result = _call_timeseries_masking_function(input_data, p_keep=p_keep)
 
     assert np.sum(result == input_data) / np.product(
         result.shape) == expected_rate
 
 
-def test_mask_contains_correct_parts_are_mean_masked():
+def test_timeseries_mask_contains_correct_parts_are_mean_masked():
     """All parts that are masked should now contain the mean of the input."""
     input_data = _get_univariate_time_series()
     mean = np.mean(input_data)
 
-    result = _call_masking_function(input_data, mask_type='mean')
+    result = _call_timeseries_masking_function(input_data, mask_type='mean')
 
     masked_parts = result[(result != input_data)]
     assert np.alltrue(
@@ -115,7 +115,7 @@ def _get_multivariate_time_series(number_of_channels: int = 6) -> np.array:
     ])
 
 
-def _call_masking_function(
+def _call_timeseries_masking_function(
     input_data,
     number_of_masks=5,
     p_keep=.3,
@@ -170,7 +170,7 @@ def test_channel_mask_masks_correct_number_of_cells():
     assert result.sum() / np.product(result.shape) == p_keep
 
 
-def test_masking_has_correct_shape_multivariate():
+def test_timeseries_masking_has_correct_shape_multivariate():
     """Test for the correct output shape for the general masking function."""
     number_of_masks = 15
     input_data = _get_multivariate_time_series()
@@ -180,7 +180,7 @@ def test_masking_has_correct_shape_multivariate():
     assert result.shape == tuple([number_of_masks] + list(input_data.shape))
 
 
-def test_masking_univariate_leaves_anything_unmasked():
+def test_timeseries_masking_univariate_leaves_anything_unmasked():
     """Tests that something remains unmasked and some parts are masked for the univariate case."""
     number_of_masks = 1
     input_data = _get_univariate_time_series()
@@ -191,7 +191,7 @@ def test_masking_univariate_leaves_anything_unmasked():
     assert np.any(~result)
 
 
-def test_masking_keep_first_instance():
+def test_timeseries_masking_keep_first_instance():
     """First instance must be the original data for Lime timeseries.
 
     Required by `lime_base` explainer, the first instance of masked (or perturbed)
