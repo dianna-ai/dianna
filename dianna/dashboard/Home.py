ad2c9a9363a4c3c97b461b9034b44d8df546a016
Laura Ootes <l.ootes@esciencecenter.nl>
2024-07-30 17:13:44 +0200

    use dynamic page import

--- dianna/dashboard/Home.py
+++ dianna/dashboard/Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 from _shared import add_sidebar_logo
 from _shared import data_directory
 from streamlit_option_menu import option_menu
-
+import importlib
 
 st.set_page_config(page_title="Dianna's dashboard",
                    page_icon='📊',
@@ -38,34 +38,44 @@ selected = option_menu(
 
 # Display the content of the selected page
 if selected == "Home":
-    st.title("Home Page")
-    st.write("Welcome to the home page of the dashboard.")
+    add_sidebar_logo()
+
+    st.image(str(data_directory / 'logo.png'))
+
+    st.markdown("""
+    DIANNA is a Python package that brings explainable AI (XAI) to your research project.
+    It wraps carefully selected XAI methods in a simple, uniform interface. It's built by,
+    with and for (academic) researchers and research software engineers working on machine
+    learning projects.
+
+    ### Pages
+
+    - <a href="/Images" target="_parent">Images</a>
+    - <a href="/Text" target="_parent">Text</a>
+    - <a href="/Time_series" target="_parent">Time series</a>
+
+
+    ### More information
+
+    - [Source code](https://github.com/dianna-ai/dianna)
+    - [Documentation](https://dianna.readthedocs.io/)
+    """,
+                unsafe_allow_html=True)
+
 else:
     # Dynamically import and execute the page
     page_module = pages[selected]
-    exec(f"import {page_module} as page")
-    exec("page.st._main()")
-
-add_sidebar_logo()
-
-st.image(str(data_directory / 'logo.png'))
-
-st.markdown("""
-DIANNA is a Python package that brings explainable AI (XAI) to your research project.
-It wraps carefully selected XAI methods in a simple, uniform interface. It's built by,
-with and for (academic) researchers and research software engineers working on machine
-learning projects.
-
-### Pages
-
-- <a href="/Images" target="_parent">Images</a>
-- <a href="/Text" target="_parent">Text</a>
-- <a href="/Time_series" target="_parent">Time series</a>
-
-
-### More information
-
-- [Source code](https://github.com/dianna-ai/dianna)
-- [Documentation](https://dianna.readthedocs.io/)
-""",
-            unsafe_allow_html=True)
+    # Make sure that all variables are reset when switching page
+    if selected != 'Images':
+        for k in st.session_state.keys():
+            if 'Image' in k:
+                st.session_state.pop(k, None)
+    if selected != 'Text':
+        for k in st.session_state.keys():
+            if 'Text' in k:
+                st.session_state.pop(k, None)
+    if selected != 'Time series':
+        for k in st.session_state.keys():
+            if 'TS' in k:
+                st.session_state.pop(k, None)
+    page = importlib.import_module(page_module)
