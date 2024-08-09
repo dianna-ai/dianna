ecb61618a97fa7b834c190d13ceedbf0f3dc1396
Laura Ootes <l.ootes@esciencecenter.nl>
2024-08-09 18:04:00 +0200

    fix imports and draft prediction

--- dianna/dashboard/pages/Tabular.py
+++ dianna/dashboard/pages/Tabular.py
@@ -1,16 +1,14 @@
 import streamlit as st
-from _image_utils import open_image
 from _model_utils import load_labels
 from _model_utils import load_model
-from _models_image import explain_image_dispatcher
-from _models_image import predict
+from _models_tabular import predict
 from _shared import _get_top_indices_and_labels
 from _shared import _methods_checkboxes
 from _shared import add_sidebar_logo
 from _shared import reset_example
 from _shared import reset_method
 from dianna.utils.downloader import download
-from dianna.visualization import plot_image
+from dianna.visualization import plot_tabular
 
 add_sidebar_logo()
 
@@ -52,5 +50,31 @@ if input_type is None:
 if not (tabular_data_file and tabular_model_file and tabular_label_file):
     st.info('Add your input data in the left panel to continue')
     st.stop()
+
+model = load_model(tabular_model_file)
+serialized_model = model.SerializeToString()
+
+labels = load_labels(tabular_label_file)
+
+choices = ('RISE', 'LIME', 'KernelSHAP')
+
+st.text("")
+st.text("")
+
+# Get predictions and create parameter box
+with st.container(border=True):
+    prediction_placeholder = st.empty()
+    methods, method_params = _methods_checkboxes(choices=choices, key='Tabular_cb')
+
+    with st.spinner('Predicting class'):
+        predictions = predict(model=serialized_model, tabular_input=tabular_data_file)
+
+    with prediction_placeholder:
+        top_indices, top_labels = _get_top_indices_and_labels(
+            predictions=predictions, labels=labels)
+
+st.text("")
+st.text("")
+
 
 st.stop()
