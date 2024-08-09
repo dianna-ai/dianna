80c284f26c45b4dbb805849bd8997c23df0201b1
Laura Ootes <l.ootes@esciencecenter.nl>
2024-08-09 22:45:32 +0200

    add table visualisation with row selection option

--- dianna/dashboard/pages/Tabular.py
+++ dianna/dashboard/pages/Tabular.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from _model_utils import load_data
 from _model_utils import load_labels
 from _model_utils import load_model
 from _models_tabular import predict
@@ -8,7 +9,10 @@ from _shared import add_sidebar_logo
 from _shared import reset_example
 from _shared import reset_method
 from dianna.utils.downloader import download
+from dianna.utils.onnx_runner import SimpleModelRunner
 from dianna.visualization import plot_tabular
+import pandas as pd
+from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
 
 add_sidebar_logo()
 
@@ -51,8 +55,12 @@ if not (tabular_data_file and tabular_model_file and tabular_label_file):
     st.info('Add your input data in the left panel to continue')
     st.stop()
 
-model = load_model(tabular_model_file)
-serialized_model = model.SerializeToString()
+data = load_data(tabular_data_file)
+
+#model = load_model(tabular_model_file)
+#serialized_model = model.SerializeToString()
+
+model = SimpleModelRunner(tabular_model_file)
 
 labels = load_labels(tabular_label_file)
 
@@ -63,15 +71,28 @@ st.text("")
 
 # Get predictions and create parameter box
 with st.container(border=True):
-    prediction_placeholder = st.empty()
+    #prediction_placeholder = st.empty()
     methods, method_params = _methods_checkboxes(choices=choices, key='Tabular_cb')
+    #prediction_placeholder = 'hello'
 
-    with st.spinner('Predicting class'):
-        predictions = predict(model=serialized_model, tabular_input=tabular_data_file)
+st.info("Select the input data either by clicking the corresponding row in the table or input the row index above to continue.")
 
-    with prediction_placeholder:
-        top_indices, top_labels = _get_top_indices_and_labels(
-            predictions=predictions, labels=labels)
+# Configure Ag-Grid options
+gb = GridOptionsBuilder.from_dataframe(data)
+gb.configure_selection('single')
+grid_options = gb.build()
+
+# Display the grid with the DataFrame
+grid_response = AgGrid(
+    data,
+    gridOptions=grid_options,
+    update_mode=GridUpdateMode.SELECTION_CHANGED,
+    theme='streamlit'
+)
+
+selected_row = grid_response['selected_rows']
+
+
 
 st.text("")
 st.text("")
