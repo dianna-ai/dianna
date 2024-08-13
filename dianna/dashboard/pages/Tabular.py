004023bfbb84f4562039c17ae175f8d86c398a49
Laura Ootes <l.ootes@esciencecenter.nl>
2024-08-13 17:11:49 +0200

    only show predicted class if classification model

--- dianna/dashboard/pages/Tabular.py
+++ dianna/dashboard/pages/Tabular.py
@@ -121,7 +121,7 @@ for col, method in zip(columns, methods):
 for index, label in zip(top_indices, top_labels):
     index_col, *columns = st.columns(column_spec)
 
-    index_col.markdown(f'##### Class: {label}')
+    if mode == 'classification': index_col.markdown(f'##### Class: {label}')
 
     for col, method in zip(columns, methods):
         kwargs = method_params[method].copy()
