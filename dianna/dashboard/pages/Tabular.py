66e907908f4a9fe511846b30d88114effb04a7cb
Laura Ootes <l.ootes@esciencecenter.nl>
2024-08-13 17:10:22 +0200

    plot relevances results

--- dianna/dashboard/pages/Tabular.py
+++ dianna/dashboard/pages/Tabular.py
@@ -134,9 +134,8 @@ for index, label in zip(top_indices, top_labels):
         with col:
             with st.spinner(f'Running {method}'):
                 relevances = func(serialized_model, selected_data, training_data, **kwargs)
-            st.stop()
-            #fig, _ = highlight_text(explanation=relevances[0], show_plot=False)
-            #st.pyplot(fig)
+            fig, _ = plot_tabular(x=relevances, y=data[:1].columns, num_features=10, show_plot=False)
+            st.pyplot(fig)
 
     # add some white space to separate rows
     st.markdown('')
