73437b5bf614027fbc62598f01bffdd8d58b35b7
Laura Ootes <l.ootes@esciencecenter.nl>
2024-08-23 13:03:54 +0200

    prep data for sunshine example

--- dianna/dashboard/_model_utils.py
+++ dianna/dashboard/_model_utils.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import numpy as np
 import onnx
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
 def load_data(file):
@@ -42,3 +43,20 @@ def load_labels(file):
 
 def load_training_data(file):
     return np.float32(np.load(file, allow_pickle=False))
+
+
+def load_train_test_sunshine(file):
+    """For the tabular sunshine example load the csv file in a pandas dataframe and split the data
+    in a train and test set."""
+    data = load_data(file)
+
+    # Drop unused columns
+    X_data = data.drop(columns=['DATE', 'MONTH', 'Index'])[:-1]
+    y_data = data.loc[1:]["BASEL_sunshine"]
+
+    # Split the data
+    X_train, X_holdout, _, y_holdout = train_test_split(X_data, y_data, test_size=0.3, random_state=0)
+    _, X_test, _, _ = train_test_split(X_holdout, y_holdout, test_size=0.5, random_state=0)
+    X_test = X_test.reset_index(drop=True)
+    X_test.insert(0, 'Index', X_test.index)
+    return X_train.to_numpy(dtype=np.float32), X_test
