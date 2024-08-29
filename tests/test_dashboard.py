a7a147141dd00c70d46cb98a0cb8b40ca287212f
Laura Ootes <l.ootes@esciencecenter.nl>
2024-08-29 15:01:00 +0200

    split test examples

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -262,6 +262,34 @@ def test_tabular_page(page: Page):
     expect(page.get_by_text("Sunshine hours prediction")).to_be_visible()
     expect(page.get_by_text("Penguin identification")).to_be_visible()
 
+    # Test using your own data
+    page.locator("label").filter(
+        has_text="Use your own data").locator("div").nth(1).click()
+    page.screenshot(path="screenshot9.png")
+    #time.sleep(6)
+    page.screenshot(path="screenshot10.png")
+    page.get_by_label("Select tabular data").get_by_test_id("baseButton-secondary").click(timeout=200_000)
+    page.screenshot(path="screenshot11.png")
+    page.get_by_label("Select model").get_by_test_id("baseButton-secondary").click()
+    page.get_by_label("Select training data").get_by_test_id("baseButton-secondary").click()
+    page.get_by_label("Select labels in case of").get_by_test_id("baseButton-secondary").click()
+
+
+def test_tabular_sunshine(page: Page):
+    """Test tabular sunshine example."""
+    page.goto(f'{BASE_URL}/Tabular')
+
+    page.get_by_text('Running...').wait_for(state='detached')
+
+    expect(page).to_have_title('Tabular')
+
+    expect(page.get_by_text("Select which input type to")).to_be_visible(timeout=100_000)
+
+    page.locator("label").filter(has_text="Use an example").locator("div").nth(1).click()
+    expect(page.get_by_text("Select an example in the left")).to_be_visible()
+    expect(page.get_by_text("Sunshine hours prediction")).to_be_visible()
+    expect(page.get_by_text("Penguin identification")).to_be_visible()
+
     # Test sunshine example
     page.locator("label").filter(has_text="Use an example").locator("div").nth(1).click()
     page.locator("label").filter(has_text="Sunshine hours prediction").locator("div").nth(1).click()
@@ -277,11 +305,11 @@ def test_tabular_page(page: Page):
     expect(page.get_by_text("Select the input data by")).to_be_visible(timeout=100_000)
     page.frame_locator("iframe[title=\"st_aggrid\\.agGrid\"]").get_by_role(
         "gridcell", name="10", exact=True).click()
-    page.get_by_text('Running...').wait_for(state='detached', timeout=100_000)
+    page.get_by_text('Running...').wait_for(state='detached', timeout=200_000)
 
     time.sleep(6)
 
-    expect(page.get_by_text("3.07")).to_be_visible(timeout=100_000)
+    expect(page.get_by_text("3.07")).to_be_visible(timeout=200_000)
 
     for selector in (
             page.get_by_role('heading', name='RISE').get_by_text('RISE'),
@@ -293,49 +321,51 @@ def test_tabular_page(page: Page):
     ):
         expect(selector).to_be_visible(timeout=100_000)
 
-    # Test penguin example
-    # Use pauses to make sure elements are loaded
+
+def test_tabular_penguin(page: Page):
+    """Test performance of tabular penguin example."""
+    page.goto(f'{BASE_URL}/Tabular')
+    page.get_by_text('Running...').wait_for(state='detached')
+
+    time.sleep(3)
+
+    expect(page).to_have_title('Tabular')
+    expect(page.get_by_text("Select which input type to")).to_be_visible(timeout=100_000)
+
+    page.locator("label").filter(has_text="Use an example").locator("div").nth(1).click()
+    expect(page.get_by_text("Select an example in the left")).to_be_visible()
+    expect(page.get_by_text("Sunshine hours prediction")).to_be_visible()
+    expect(page.get_by_text("Penguin identification")).to_be_visible()
+
+    # Test sunshine example
     page.locator("label").filter(has_text="Use an example").locator("div").nth(1).click()
     page.locator("label").filter(has_text="Penguin identification").locator("div").nth(1).click()
-
-    expect(page.get_by_text("Select a method to continue")).to_be_visible(timeout=200_000)
-
-    time.sleep(3)
-
-    page.locator("label").filter(has_text="RISE").locator("span").click()
-    page.locator("label").filter(has_text="LIME").locator("span").click()
-    page.locator("label").filter(has_text="KernelSHAP").locator("span").click()
+    expect(page.get_by_text("Select a method to continue")).to_be_visible(timeout=100_000)
 
     time.sleep(3)
 
-    expect(page.get_by_text("Select the input data by")).to_be_visible()
-    page.get_by_text('Running...').wait_for(state='detached', timeout=200_000)
+    page.locator("label").filter(has_text="RISE").locator("span").click(timeout=300_000)
+    page.locator("label").filter(has_text="LIME").locator("span").click(timeout=300_000)
+    page.locator("label").filter(has_text="KernelSHAP").locator("span").click(timeout=300_000)
 
     time.sleep(3)
 
-    page.frame_locator('iframe[title=\"st_aggrid\\.agGrid\"]').get_by_role(
-        'gridcell', name='10', exact=True).click(timeout=200_000)
-    page.get_by_text('Running...').wait_for(state='detached', timeout=100_000)
+    expect(page.get_by_text("Select the input data by")).to_be_visible(timeout=300_000)
+    page.frame_locator("iframe[title=\"st_aggrid\\.agGrid\"]").get_by_role(
+        "gridcell", name="10", exact=True).click()
+    page.get_by_text('Running...').wait_for(state='detached', timeout=300_000)
 
-    time.sleep(10)
+    time.sleep(6)
 
     for selector in (
-            page.get_by_text('Predicted class:'),
-            page.get_by_test_id('stMetricValue').get_by_text('Gentoo'),
-            page.get_by_role('heading', name='RISE').get_by_text('RISE'),
-            page.get_by_role('heading', name='KernelSHAP').get_by_text('KernelSHAP'),
-            page.get_by_role('heading', name='LIME').get_by_text('LIME'),
-            page.get_by_role('img', name='0').first,
-            page.get_by_role('img', name='0').nth(1),
-            page.get_by_role('img', name='0').nth(2),
+        page.get_by_text('Predicted class:'),
+        page.get_by_test_id('stMetricValue').get_by_text('Gentoo'),
+        page.get_by_role('heading', name='RISE').get_by_text('RISE'),
+        page.get_by_role('heading', name='KernelSHAP').get_by_text('KernelSHAP'),
+        page.get_by_role('heading', name='LIME').get_by_text('LIME'),
+        page.get_by_role('img', name='0').first,
+        page.get_by_role('img', name='0').nth(1),
+        page.get_by_role('img', name='0').nth(2),
     ):
-        expect(selector).to_be_visible(timeout=200_000)
+        expect(selector).to_be_visible(timeout=100_000)
 
-    # Test using your own data
-    page.locator("label").filter(
-        has_text="Use your own data").locator("div").nth(1).click()
-    time.sleep(6)
-    page.get_by_label("Select tabular data").get_by_test_id("baseButton-secondary").click(timeout=200_000)
-    page.get_by_label("Select model").get_by_test_id("baseButton-secondary").click()
-    page.get_by_label("Select training data").get_by_test_id("baseButton-secondary").click()
-    page.get_by_label("Select labels in case of").get_by_test_id("baseButton-secondary").click()
