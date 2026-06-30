56d7b27baa33ab5ee6fee0d3f6fc38b704df4772
E. G. Patrick Bos <egpbos@gmail.com>
2026-06-30 09:36:44 +0200

    bump patch version

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = u'DIANNA Team'
 # built documents.
 #
 # The short X.Y version.
-version = '1.8.0'
+version = '1.8.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
